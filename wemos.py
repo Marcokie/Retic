43110ad7c90c409f3134d988e2165e02d0e2a76a
Marcokie <marco.vanstaden@gmail.com>
2018-10-13 06:32:45 +0800

    Refactor

--- wemos.py
+++ wemos.py
@@ -1,15 +1,21 @@
 # Import Libraries
-import utime
 import conf
+import network
+import utime
+from machine import Pin
+import ubinascii
+from umqtt.simple import MQTTClient
+
 # Set parameters
 my_ssid = conf.wifi["ssid"]
 my_pwd = conf.wifi["pwd"]
+SERVER = conf.adafruit["server"]
+TOPIC = conf.adafruit["topic"]
+
+
 # functions
-
-
 # Connect to wifi
 def do_connect(ssid,pwd):
-    import network
     sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
         print('connecting to network...')
@@ -21,13 +27,36 @@ def do_connect(ssid,pwd):
 
 
 # Setup Mqtt
+def sub_cb(topic, msg):
+    print((topic, msg))
 
 
-# main
-def main(state):
-    from machine import Pin
+# Main function
+def main():
+    # Connect to wifi
     do_connect(my_ssid,my_pwd)
+
+    # Print MQTT message
+    print("Connecting to %s, subscribed to %s topic" % (SERVER, TOPIC))
+    CLIENT_ID = ubinascii.hexlify(machine.unique_id())
+    c = MQTTClient(CLIENT_ID, SERVER)
+    c.set_callback(sub_cb)
+    c.connect()
+    c.subscribe(TOPIC)
+    print("Connected to %s, subscribed to %s topic" % (SERVER, TOPIC))
+
+    try:
+        while 1:
+            #micropython.mem_info()
+            c.wait_msg()
+    finally:
+        c.disconnect()
+
+    # Toggle LED
     led = Pin(2, Pin.OUT, value=1)
     led.value(state)
     state = 1 - state
-main(0)
+
+# Run main function
+if __name__ == __main__:
+    main()

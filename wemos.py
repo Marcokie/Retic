21883e0e7fcdf9e2a14719f6f68cffb86e4f67f4
Marcokie <marco.vanstaden@gmail.com>
2018-10-21 16:13:46 +0800

    Hnadle stop, correct json and ignores non-json statements

--- wemos.py
+++ wemos.py
@@ -8,7 +8,12 @@ import ubinascii
 from umqtt.simple import MQTTClient
 import json
 
-# Set global parameters
+
+# Define classes
+class CustomStop(Exception):
+    pass
+
+
 # Wifi params
 my_ssid = conf.wifi["ssid"]
 my_pwd = conf.wifi["pwd"]
@@ -23,8 +28,7 @@ led = Pin(2, Pin.OUT, value=1)
 # Set gpio dictionary
 gpio = {}
 for k,v in conf.zones.items():
-    gpio[k] = [Pin(v, Pin.OUT,value=0),0]  # Dictionary of lists. [0] = assigned pin, [1] = timer minutes
-
+    gpio[k] = [Pin(v, Pin.OUT,value=1),0]  # Dictionary of lists. [0] = assigned pin, [1] = timer minutes
 
 # functions
 # Connect to wifi
@@ -38,47 +42,48 @@ def do_connect(ssid,pwd):
             utime.sleep_ms(200)
     print('network config:', sta_if.ifconfig())
 
-
 # Setup Mqtt
 def sub_cb(topic, msg):
     print((topic, msg))
     if msg == b'STOP':
-        stop = True
+        print("Stop wemos")
+        raise CustomStop
     else:
         try:
-            jmsg = json.loads(msg)  #load json message
+            jmsg = json.loads(msg)  # load json message
             for k,v in jmsg.items():
                 gpio[k][1] = v
             print(gpio)
-        except Exception as e:
-            print("mqqt message error: " + e)
+        except ValueError as e:
+            return
 
 
 # Main function
 def main():
-    # Initial setup
-    stop = False
     # Subscribe to MQTT message
     c = MQTTClient(CLIENT_ID, SERVER, user=USER, password=KEY, port=1883)
     c.set_callback(sub_cb)
     c.connect()
     c.subscribe(TOPIC)
     # [Re]connect Loop
-    print(sta_if.isconnected())
+    print("Connection status %s" % sta_if.isconnected())
     try:
         while not sta_if.isconnected():
             # Connect to wifi
             do_connect(my_ssid,my_pwd)
             print("Connected to %s, subscribed to %s topic" % (SERVER, TOPIC))
-        while not stop:
+        while 1:
+            print("checking messages")
             c.check_msg()  # execute call back to field timer requests
             for x in gpio.values():  # Set pin values
                 if x[1] > 0:
-                    x[0].value(1)
-                else:
                     x[0].value(0)
-            utime.wait(5)
-            for x in gpio.values():  #decrease timers
+                else:
+                    x[0].value(1)
+            print(gpio)
+            utime.sleep(5)
+            print("Decreasing pin values")
+            for x in gpio.values():  # decrease timers
                 x[1] = max(0,x[1]-5)
     except Exception as e:
         print(e)
